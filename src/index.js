9a62a725f244fd05fe2ea056338b08922947d7f6
Thomas Jennifer <jsamuelt3@gmail.com>
2023-12-16 18:45:37 +0100

    integrated API with the forecast template

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function updateWeather(response) {
   let temperature = response.data.temperature.current;
   let h1 = document.querySelector("#city");
   h1.innerHTML = response.data.city;
-  console.log(response.data);
+
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
@@ -16,6 +16,8 @@ function updateWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   timeElement.innerHTML = formatDate(date);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="weather-icon" />`;
+
+  getForecast(response.data.city);
 
   temperatureElement.innerHTML = Math.round(temperature);
 }
@@ -51,30 +53,50 @@ function handleSearch(event) {
 
   searchCity(searchElement.value);
 }
-function displayForecast() {
-  let days = ["Mon", "Tue", "Wed", "Thur", "Fri"];
+
+function getForecast(city) {
+  let apiKey = "7fa2303eo545aaff0tfbf93c4ef484a6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return day[date.getDay()];
+}
+
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      ` 
-              <div class="weather-forecast-date">${day}
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        ` 
+              <div class="weather-forecast-date">${formatDay(day.time)}
+              
               <div>
                 <img
-                  src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAQBJREFUaN7t2csNwyAMBmBGYYSMwhgdgxEYjRW6ARu4HNyqB0CKednElf5b2/hLSALGAICRHKMABSjgUMDdD7xfLifkxByoJOJ33O3/nwHIhVgsKDWKriXhb+0WQD6wJxZegvhlADzrcUDhpeFlpwLyAa5BZ711Na4pgAXFNxFdABw2K4r/R9iRgLiw+N89MQSATxvYFN8F2DB0qkOJCggbi/8m9AASA0AiAXBuA0ziKIDACBAogMgIECkAYBUFKEABzwOIf4yKf5HJnkqIn8wxmk775y5oxC8pj1jUH9FWEd/YOqK1eERz94j2euFqUCF7NzjYbzHpLqUCFKCAJfkAq7RimK7qUtAAAAAASUVORK5CYII="
+                  src="${day.condition.icon_url}"
                   class="forecast-icon"
                 />
               </div>
               <div class="weather-temperatures">
                 <div>
-                  <strong class="max-forecast-temperature">32°</strong>
-                  <span class="min-forecast-temperature">26°</span>
+                  <strong class="max-forecast-temperature">${Math.round(
+                    day.temperature.maximum
+                  )}°</strong>
+                  <span class="min-forecast-temperature">${Math.round(
+                    day.temperature.minimum
+                  )}°</span>
                 </div>
               </div>
               </div>
           
           `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
